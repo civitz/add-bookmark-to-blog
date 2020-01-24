1cc268472c887c1b73e8a0fbf24785e4e4cffd31
Roberto Piva <piva.roberto.88@gmail.com>
2020-01-24 22:53:44 +0000

    add command line interface

--- add-bookmark-to-blog.py
+++ add-bookmark-to-blog.py
@@ -2,6 +2,8 @@ from github import Github
 from datetime import date
 import os
 import yaml
+import sys
+import argparse
 
 # or using an access token
 
@@ -14,7 +16,6 @@ def get_blog (github_login):
     return github_login.get_repo("civitz/civitz.github.io")
 
 def file_content(link, description, short=None, publish=True, tags=[]):
-    yfm="---\n"
     parts={
         'published': publish,
         'link': link
@@ -23,24 +24,60 @@ def file_content(link, description, short=None, publish=True, tags=[]):
         parts['short']=short
     if tags != None and tags:
         parts['tags']=tags
+    print (parts)
     yaml_front_matter = yaml.dump(parts)
     return "---\n" + yaml_front_matter + "---\n\n" + description + "\n"
 
 def file_name(date, name):
     return date.strftime("%Y-%m-%d-") + name.replace(" ","-") + ".md"
 
-
-def add_link_to_blog(link, name, description, short=None, publish=True, tags=[])
-    print("logging in")
-    gh=login()
-    blog=get_blog(gh)
-    filename = file_name(date.today(), name.lower()))
+def add_link_to_blog(link, name, description, short=None, publish=True, tags=[], dryrun=False):
+    filename = file_name(date.today(), name.lower())
     print ("Resulting file name: " + filename)
-    content = file_content(link,description, publish=False)
-    print ("Resulting file content: " + content)
-    result = blog.create_file(path="_bookmarks/"+filename, content=content,branch="netlify",message="Adding " + filename)
-    print ("Result: ")
-    print result
+    content = file_content(link,description, publish=publish,short=short, tags=tags)
+    print ("Resulting file content: ")
+    print (content)
+    if dryrun:
+        print ("Here we would have created a file on the blog")
+    else:
+        print("logging in")
+        gh=login()
+        blog=get_blog(gh)
+        result = blog.create_file(path="_bookmarks/"+filename, content=content,branch="netlify",message="Adding " + filename)
+        print ("Result: ")
+        print (result)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("link",
+                        help="the link to publish")
+    parser.add_argument("-n", "--name",
+                        help="file name")
+    parser.add_argument("-s", "--short",
+                        help="Short name for page")
+    parser.add_argument("-d", "--description",
+                        help="Link description")
+    parser.add_argument("-t", "--tags", action="append",
+                        help="Tag list")
+    parser.add_argument("-p", "--publish", action="store_true",
+                        help="immediately publish")
+    parser.add_argument("--dryrun", action="store_true",
+                        help="don't publish to github")
+    parsed = parser.parse_args(argv)
+    print parsed
+    if parsed.description == None:
+        print ("Missing description parameter")
+        sys.exit(1)
+    if parsed.name == None:
+        print ("Missing name parameter")
+        sys.exit(1)
+    add_link_to_blog(link=parsed.link, name=parsed.name,
+                     description=parsed.description, short=parsed.short,
+                     publish=parsed.publish, tags=parsed.tags, dryrun=parsed.dryrun)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 def lambda_handler(event, context):
     print('## EVENT')
